87b9b557ebdf25638e4b92303a301256b5146c93
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-05-23 12:00:06 +0200

    Initial layout/styling of ProductNav component.

--- src/components/ProductNav.jsx
+++ src/components/ProductNav.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Wrapper = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
+  width: 100%;
+  background: #FFF;
+  border-bottom: 1px solid #d5d6d6;
+`;
+
+const NavButtons = styled.ul`
+  display: flex;
+`;
+
+const NavButton = styled.li`
+  font-size: 14px;
+  border-right: 2px solid #d5d6d6;
+  padding: 16.5px 20px;
+  color: #2E3134;
 `;
 
 function ProductNav() {
   return (
     <Wrapper>
-      <h1>ProductNav</h1>
+      <NavButtons>
+        <Link to="#">
+          <NavButton>Blog</NavButton>
+        </Link>
+        <Link to="#">
+          <NavButton>Nourish</NavButton>
+        </Link>
+        <Link to="#">
+          <NavButton>Shop</NavButton>
+        </Link>
+      </NavButtons>
     </Wrapper>
-  )
+  );
 }
 
 export default ProductNav;
