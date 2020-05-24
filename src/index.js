fd1742efdc0520ab928311c5f46d7825f1131f8b
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-05-24 21:22:51 +0200

    Prepare for deploy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "core-js/es/map";
+import "core-js/es/set";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
